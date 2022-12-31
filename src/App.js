3501c6de2a80f313903d32069a7530948cf3960d
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2022-12-31 02:00:11 +0000

    Delete default react code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
-      <h1></h1>
+      <h1>My Porfolio</h1>
       </header>
     </div>
   );
