b71c46ec64c57a5389523cd38c9a564a15a58fc4
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-01 03:29:55 +0000

    Import Blobs,js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Blobs from './components/dep/Blobs';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
+        <Blobs />
         <h1>My Porfolio</h1>
       </header>
     </div>
