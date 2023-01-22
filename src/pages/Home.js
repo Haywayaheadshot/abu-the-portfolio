b5146b1baaf91eaaf050293947f794398ab2278d
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-22 16:29:12 +0000

    Add about, contact, projects and home page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to my page</h1>
+    </div>
+  );
+}
+
+export default Home;
