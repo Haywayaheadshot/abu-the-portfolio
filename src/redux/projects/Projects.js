29f67137ccd19589fec70404b438c920a1aca5cd
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-03-05 19:05:34 +0000

    Fix linter errors

--- src/redux/projects/Projects.js
+++ src/redux/projects/Projects.js
@@ -1,20 +1,19 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import axios from 'axios';
 
-initialState: [
-    
-],
-const greetingsSlice = createSlice({
-  name: 'projects',
-  extraReducers: (builder) => {
-    builder.addCase(fetchGreetings.fulfilled, (_, action) => action.payload);
-  },
-});
+// defaultstate =
+// const greetingsSlice = createSlice({
+//   name: 'projects',
+//   initialState: [],
+//   extraReducers: (builder) => {
+//     builder.addCase(fetchGreetings.fulfilled, (_, action) => action.payload);
+//   },
+// });
 
-export const fetchGreetings = createAsyncThunk('FETCH_MESSAGE', () => axios.get(url)
-  .then((response) => {
-    const greeting = response.data.message;
-    return greeting;
-}));
+// export const fetchGreetings = createAsyncThunk('FETCH_MESSAGE', () => axios.get(url)
+//   .then((response) => {
+//     const greeting = response.data.message;
+//     return greeting;
+//   }));
 
-export default greetingsSlice.reducer;
+// export default greetingsSlice.reducer;
