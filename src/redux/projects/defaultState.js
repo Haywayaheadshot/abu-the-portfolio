bc3bb34e622d9b5a35d68bec4f3a81775bf21e58
USER <user@Apples-MacBook-Pro.local>
2023-03-25 17:39:13 +0000

    Succeffully separate defaultState into different file

--- src/redux/projects/defaultState.js
+++ src/redux/projects/defaultState.js
@@ -3,12 +3,31 @@ import { v4 as uuidv4 } from 'uuid';
 const defaultState = [
   {
     id: uuidv4(),
-    title: 'Legend Of The Seeker',
-    author: 'Terry Goodkind',
-    progress: '64%',
-    currentStatus: '17',
-    genre: 'Thriller',
+    title: 'Book an Appointment',
+    description:
+    `
+    Book an Appointment is a project based on an app to book an appointment with a doctor.
+    This is the front-end which was connected to a back-end repo.
+    `,
+    liveDemo: 'https://book-doctors-appointment.netlify.app/',
+    sourceCode: 'https://github.com/Haywayaheadshot/book-an-appointment-front-end',
+    stack: 'frontEnd, fullStack',
+    builtWith: ['React', 'Redux'],
+    mobileScreenShot: '../components/assets/doctors-appointment/Doctors-Appointment-front-end-mobile.png',
   },
+//   {
+//     id: uuidv4(),
+//     title: 'Book an Appointment',
+//     description:
+//     `
+//     Book an Appointment is a project based on an app to book an appointment with a doctor.
+//     This is the back-end which was connected to a front-end repo.
+//     `,
+//     liveDemo: 'https://book-doctors-appointment.netlify.app/',
+//     sourceCode: 'https://github.com/Haywayaheadshot/book-an-appointment',
+//     stack: 'backEnd, fullStack',
+//     builtWith: ['Ruby', 'Ruby on Rails'],
+//   },
 ];
 
 export default defaultState;
