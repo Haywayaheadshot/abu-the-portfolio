25b55f318a110a767fac495e37b0845fc79bc9dc
USER <user@Apples-MacBook-Pro.local>
2023-03-27 17:20:00 +0000

    Succeffully map all skills and levels

--- src/components/dep/Skill.js
+++ src/components/dep/Skill.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../../styles/skill.css';
 
 function Skill({
-  title,
+  title, skillArray,
 }) {
   return (
-    <div>{title}</div>
+    <ul className="proj-ul">
+      <li>
+        <h1>
+          {title}
+        </h1>
+      </li>
+      <li className="skill-list-container">
+        {skillArray.map((skill) => (
+          <ul className="skill-ul" key={skill.id}>
+            <li>
+              <h2>
+                {skill.skill}
+                <br />
+                <span className="level-span">
+                  {skill.level}
+                </span>
+              </h2>
+            </li>
+          </ul>
+        ))}
+      </li>
+    </ul>
   );
 }
 
@@ -13,9 +35,9 @@ export default Skill;
 
 Skill.propTypes = {
   title: PropTypes.string.isRequired,
+  skillArray: PropTypes.arrayOf(PropTypes.string).isRequired,
   // mobileSrc: PropTypes.string.isRequired,
   // description: PropTypes.string.isRequired,
-  // builtWith: PropTypes.arrayOf(PropTypes.string).isRequired,
   // liveDemo: PropTypes.string.isRequired,
   // sourceCode: PropTypes.string.isRequired,
   // deskScreenShot: PropTypes.string.isRequired,
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Skill from '../components/dep/Skill';
+import '../styles/skills.css';
 
 function Skills() {
   const skill = useSelector((state) => state.skills);
   return (
-    <div>
-      <h1>My Skills</h1>
+    <div className="skills-container">
+      <h1>Skills</h1>
       {skill.map((s) => (
-        <section key={s.id}>
+        <section key={s.id} className="skills-display" data-testid="skill">
           <Skill
             title={s.title}
+            skillArray={s.skillArray}
           />
         </section>
       ))}
