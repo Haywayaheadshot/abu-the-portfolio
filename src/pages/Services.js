f5a6dec4c0d8ebc3adc021903220ffb164f06156
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-04-06 15:18:20 +0000

    map services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,17 +1,41 @@
 import React from 'react';
-// import
+import { useSelector } from 'react-redux';
+import * as FaIcons from 'react-icons/fa';
+import * as SiIcons from 'react-icons/si';
+import '../styles/services.css';
 
 const Services = () => {
-  const service = 'x';
-  console.log(service);
+  const services = useSelector((state) => state.services);
+
+  const getIconComponent = (iconName) => {
+    const IconComponent = FaIcons[iconName] || SiIcons[iconName];
+    if (!IconComponent) {
+      // eslint-disable-next-line no-console
+      console.warn(`Invalid icon name: ${iconName}`);
+    }
+    return IconComponent;
+  };
+
   return (
     <div className="services-container">
       <section>
         <h1>Services</h1>
         <h4>What I offer</h4>
       </section>
-      {/* // <section>
-      // </section> */}
+      <section className="mapped-services">
+        {services.map((service) => {
+          const IconComponent = getIconComponent(service.icon);
+          return (
+            <div key={service.id} className="service-div">
+              {IconComponent && <IconComponent size={40} />}
+              <h3>{service.name}</h3>
+              <button type="button">
+                View more ↗️
+              </button>
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 };
