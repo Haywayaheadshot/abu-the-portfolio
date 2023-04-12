cc3722f4f1633e6055fbfde7aa4ef9b0a5bfbdae
Pragyan <pragyanpatidar14@gmail.com>
2023-04-12 21:37:34 +0530

    landing page typo fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -42,7 +42,7 @@ const Home = () => (
         I can help you build a product, feature or website
         Look through some of my work and experience!
         If you like what you see and have a project you need coded,
-        try not to hestiate to contact me.
+        try not to hesitate to contact me.
       </p>
     </section>
   </div>
