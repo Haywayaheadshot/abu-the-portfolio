7bcf0d98f3a526f3a9993870f7e5020c59689f2a
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-04-17 16:17:02 +0000

    Fix contact form submit error

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,28 +5,30 @@ const Contact = () => {
   const [errors, setErrors] = useState({});
 
   const handleFormSubmit = (event) => {
-    event.preventDefault();
-
     const { name, email, message } = event.target.elements;
     const newErrors = {};
 
     if (!name.value.trim()) {
+      event.preventDefault();
       newErrors.name = 'Please input your name';
     }
 
     if (!email.value.trim() || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+      event.preventDefault();
       newErrors.email = 'Please input a valid email address';
     }
 
     if (!message.value.trim()) {
+      event.preventDefault();
       newErrors.message = 'Please input a message';
     }
 
     setErrors(newErrors);
 
     if (Object.keys(newErrors).length === 0) {
-      event.target.submit();
+      return true; // allow form submission
     }
+    return false; // prevent form submission
   };
 
   return (
@@ -37,21 +39,21 @@ const Contact = () => {
           <label htmlFor="name">
             Name
             <br />
-            <input type="text" placeholder="Name" id="name" />
+            <input type="text" placeholder="Name" id="name" name="name" />
           </label>
           {errors.name && <span className="contact-error-message">{errors.name}</span>}
           <br />
           <label htmlFor="email">
             Email
             <br />
-            <input type="email" placeholder="Email" id="email" />
+            <input type="email" placeholder="Email" id="email" name="email" />
           </label>
           {errors.email && <span className="contact-error-message">{errors.email}</span>}
           <br />
           <label htmlFor="message">
             Project
             <br />
-            <textarea placeholder="Message" id="message" />
+            <textarea placeholder="Message" id="message" name="message" />
           </label>
           {errors.message && <span className="contact-error-message">{errors.message}</span>}
           <section className="contact-submit-btn-div">
